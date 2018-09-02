911ccb164878b2c69b409f300f9ba8304fb3974c
RELNO <noyman@Ariels-MacBook-Pro.local>
2018-09-01 22:37:47 -0400

    color finidng WIP

--- SOFTWARE/SCANNER/py/SCANNER.py
+++ SOFTWARE/SCANNER/py/SCANNER.py
@@ -2,9 +2,9 @@ import math
 import numpy as np
 import cv2
 
-# define range of blue color in HSV
-lower_blue = np.array([110, 50, 50])
-upper_blue = np.array([130, 255, 255])
+# define range of  color in HSV
+ORANGE_MIN = np.array([5, 50, 50], np.uint8)
+ORANGE_MAX = np.array([15, 255, 255], np.uint8)
 
 # load the keystone data from file
 M = np.loadtxt('keystone.txt')
@@ -20,7 +20,7 @@ gridSize = 10
 cropSize = 10
 # array to collect the scanners
 colorArr = np.tile(np.array([0.]), (gridSize, gridSize))
-print(colorArr)
+
 # run the video loop forever
 while(True):
     _, frame = webcam.read()
@@ -31,22 +31,24 @@ while(True):
         for y in range(0, vidRes-cropSize, int(vidRes/gridSize)):
             crop = dst[y:y+cropSize, x:x+cropSize]
             # draw rects with mean value of color
-            cvMeanCol = cv2.mean(crop)
-            # if cvMeanCol cv2.inRange(hsv, lower_green, upper_green):
-                pass
-            # print('\n', cvMeanCol)
-            # colorArr[x, y] = cvMeanCol
-            # make frame around crop
+            meanCol = cv2.mean(crop)
+            cropOne = dst[y:y+1, x:x+1]
+            bgrToHsv = cv2.cvtColor(cropOne, cv2.COLOR_BGR2HSV)
+            inRange = cv2.inRange(bgrToHsv, ORANGE_MIN, ORANGE_MAX)
+            inRange = int(inRange)
+            # print(inRange, type(inRange))
+            #########
             cv2.rectangle(dst, (x-1, y-1), (x+cropSize + 1, y+cropSize + 1),
-                          (0, 255, 0), 1)
+                          (inRange, inRange, inRange), 1)
             # then draw the mean color itself
             cv2.rectangle(dst, (x, y), (x+cropSize,
-                                        y+cropSize), cvMeanCol, -1)
-            np.append(colorArr, cvMeanCol)
+                                        y+cropSize), meanCol, -1)
+            np.append(colorArr, meanCol)
 
     # print('\n', colorArr)
     # draw the video to screen
     cv2.imshow("vid", dst)
+
     key = cv2.waitKey(10) & 0xFF
     if key == 27:
         break
