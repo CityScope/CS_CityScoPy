ea8704bc0cb6663f6cea5eefb49bd5be22331626
RELNO <noyman@Ariels-MacBook-Pro.local>
2018-09-02 22:37:00 -0400

    reshape result array to HxW

--- SOFTWARE/SCANNER/py/KEYSTONE.py
+++ SOFTWARE/SCANNER/py/KEYSTONE.py
@@ -1,3 +1,34 @@
+# ////////////////////////////////////////////////////////////////////////////////////
+# {{ CityScopePy }}
+# Copyright (C) {{ 2018 }}  {{ Ariel Noyman }}
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# ////////////////////////////////////////////////////////////////////////////////////
+
+# CityScopePy KEYSTONE
+# Exports a selection of a transformed matrix from a webcam video using GUI
+
+# "@context": "https://github.com/CityScope/", "@type": "Person", "address": {
+# "@type": "75 Amherst St, Cambridge, MA 02139", "addressLocality":
+# "Cambridge", "addressRegion": "MA",},
+# "jobTitle": "Research Scientist", "name": "Ariel Noyman",
+# "alumniOf": "MIT", "url": "http://arielnoyman.com",
+# "https://www.linkedin.com/", "http://twitter.com/relno",
+# https://github.com/RELNO]
+
+
 import numpy as np
 import cv2
 
--- SOFTWARE/SCANNER/py/SCANNER.py
+++ SOFTWARE/SCANNER/py/SCANNER.py
@@ -1,3 +1,37 @@
+# /////////////////////////////////////////////////////////////////////////////
+# {{ CityScopePy }}
+# Copyright (C) {{ 2018 }}  {{ Ariel Noyman }}
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# /////////////////////////////////////////////////////////////////////////////
+
+# CityScopePy SCANNER
+# a simple decoder of 2d array made of LEGO bricks, parsing and sending for visualzation
+
+# "@context": "https://github.com/CityScope/", "@type": "Person", "address": {
+# "@type": "75 Amherst St, Cambridge, MA 02139", "addressLocality":
+# "Cambridge", "addressRegion": "MA",},
+# "jobTitle": "Research Scientist", "name": "Ariel Noyman",
+# "alumniOf": "MIT", "url": "http://arielnoyman.com",
+# "https://www.linkedin.com/", "http://twitter.com/relno",
+# https://github.com/RELNO]
+
+
+# raise SystemExit(0)
+
+
 import math
 import numpy as np
 import cv2
@@ -10,21 +44,19 @@ webcam = cv2.VideoCapture(0)
 # define the video window
 cv2.namedWindow('vid')
 # set res. for vid
-videoRes = 500
+videoRes = 600
 # define the number of grid scanners
-gridSize = 10
+gridSize = 20
 # define the size for each scanner
-cropSize = 5
+cropSize = 10
 # array to collect the scanners
-colorArr = np.zeros((3, 2, 2), dtype=np.int64)
-
-print(colorArr)
-# raise SystemExit(0)
-
+colorArr = np.zeros((gridSize*gridSize), dtype=np.int64)
 colors = MODULES.colDict
+
 
 # run the video loop forever
 while(True):
+    counter = 0
     _, frame = webcam.read()
     dst = cv2.warpPerspective(
         frame, M, (videoRes, videoRes))
@@ -50,12 +82,17 @@ while(True):
             cv2.rectangle(dst, (x, y), (x+cropSize,
                                         y+cropSize), meanCol, -1)
 
+            cv2.putText(dst, str([counter, scannerCol]),
+                        (x-2, y-2), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.3, (0, 0, 0), lineType=cv2.LINE_AA)
             # add colors to array for type analysis
-            # colorArr[x][y] = scannerCol
+            colorArr[counter] = scannerCol
+            counter += 1
 
+    print('\n', '_______', '\n',
+          colorArr.reshape(gridSize, gridSize).transpose())
     # draw the video to screen
     cv2.imshow("vid", dst)
-
     # break video loop by pressing ESC
     key = cv2.waitKey(10) & 0xFF
     if key == 27:
