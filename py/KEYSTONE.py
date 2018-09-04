9dc61c7b9a9c4bfc1b8da3c01f2f97644a8391f8
RELNO <noyman@ariels-mbp.media.mit.edu>
2018-09-04 09:45:13 -0400

    dir reset

---
+++ py/KEYSTONE.py
@@ -0,0 +1,105 @@
+# ////////////////////////////////////////////////////////////////////////////////////
+# {{ CityScopePy }}
+# Copyright (C) {{ 2018 }}  {{ Ariel Noyman }}
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# ////////////////////////////////////////////////////////////////////////////////////
+
+# CityScopePy KEYSTONE
+# Exports a selection of a transformed matrix from a webcam video using GUI
+
+# "@context": "https://github.com/CityScope/", "@type": "Person", "address": {
+# "@type": "75 Amherst St, Cambridge, MA 02139", "addressLocality":
+# "Cambridge", "addressRegion": "MA",},
+# "jobTitle": "Research Scientist", "name": "Ariel Noyman",
+# "alumniOf": "MIT", "url": "http://arielnoyman.com",
+# "https://www.linkedin.com/", "http://twitter.com/relno",
+# https://github.com/RELNO]
+
+
+import numpy as np
+import cv2
+
+webcam = cv2.VideoCapture(0)
+cv2.namedWindow('image')
+webcam.set(3, 800)
+webcam.set(4, 600)
+
+# top left, top right, bottom left, bottom right
+pts = [(0, 0), (0, 0), (0, 0), (0, 0)]
+pointIndex = 0
+mousePos = (0, 0)
+
+# Aspect ratio
+ASPECT_RATIO = (600, 600)
+pts2 = np.float32([[0, 0], [ASPECT_RATIO[1], 0], [0, ASPECT_RATIO[0]], [
+                  ASPECT_RATIO[1], ASPECT_RATIO[0]]])
+
+
+def selectFourPoints():
+    global frame
+    global pointIndex
+
+    print("select 4 points, by double clicking on each of them in the order: \n\
+	top left, top right, bottom left, bottom right.")
+    while(pointIndex != 4):
+        # wait for clicks
+        cv2.setMouseCallback('image', saveThisPoint)
+        _, frame = webcam.read()
+        # draw mouse pos
+        cv2.circle(frame, mousePos, 10, (0, 0, 255), 1)
+        # draw clicked points
+        for pt in pts:
+            cv2.circle(frame, pt, 10, (255, 0, 0), 1)
+        cv2.imshow('image', frame)
+        key = cv2.waitKey(20) & 0xFF
+        if key == 27:
+            return False
+    return True
+
+
+def saveThisPoint(event, x, y, flags, param):
+    # mouse callback function
+    global frame
+    global pointIndex
+    global pts
+    global mousePos
+
+    if event == cv2.EVENT_MOUSEMOVE:
+        mousePos = (x, y)
+    elif event == cv2.EVENT_LBUTTONUP:
+        # draw a ref. circle
+        print('point  # ', pointIndex, (x, y))
+        # save this point to the array pts
+        pts[pointIndex] = (x, y)
+        pointIndex = pointIndex + 1
+
+
+# checks if finished selecting the 4 corners
+if(selectFourPoints()):
+    # The four points in the image
+    pts1 = np.float32([
+        [pts[0][0], pts[0][1]],
+        [pts[1][0], pts[1][1]],
+        [pts[2][0], pts[2][1]],
+        [pts[3][0], pts[3][1]]])
+
+# perform the transformation
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+    print("np array keystone pts", M)
+    np.savetxt("keystone.txt", M)
+
+webcam.release()
+cv2.destroyAllWindows()
---
+++ py/SCANNER.py
@@ -0,0 +1,102 @@
+# /////////////////////////////////////////////////////////////////////////////
+# {{ CityScopePy }}
+# Copyright (C) {{ 2018 }}  {{ Ariel Noyman }}
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# /////////////////////////////////////////////////////////////////////////////
+
+# CityScopePy SCANNER
+# a simple decoder of 2d array made of LEGO bricks, parsing and sending for visualzation
+
+# "@context": "https://github.com/CityScope/", "@type": "Person", "address": {
+# "@type": "75 Amherst St, Cambridge, MA 02139", "addressLocality":
+# "Cambridge", "addressRegion": "MA",},
+# "jobTitle": "Research Scientist", "name": "Ariel Noyman",
+# "alumniOf": "MIT", "url": "http://arielnoyman.com",
+# "https://www.linkedin.com/", "http://twitter.com/relno",
+# https://github.com/RELNO]
+
+
+# raise SystemExit(0)
+
+
+import math
+import numpy as np
+import cv2
+import MODULES
+
+# load the keystone data from file
+M = np.loadtxt('keystone.txt')
+# define the video
+webcam = cv2.VideoCapture(0)
+# define the video window
+cv2.namedWindow('vid')
+# set res. for vid
+videoRes = 800
+# define the number of grid scanners
+gridSize = 20
+# define the size for each scanner
+cropSize = 10
+# array to collect the scanners
+colorArr = np.zeros((gridSize*gridSize), dtype=np.int64)
+colors = MODULES.colDict
+
+
+# run the video loop forever
+while(True):
+    counter = 0
+    _, frame = webcam.read()
+    dst = cv2.warpPerspective(
+        frame, M, (videoRes, videoRes))
+
+    # if needed, implement max_rgb_filter
+    # dst = MODULES.max_rgb_filter(dst)
+    step = int(videoRes/gridSize)
+    for x in range(int(step/2), videoRes, step):
+        for y in range(int(step/2), videoRes, step):
+            crop = dst[y:y+cropSize, x:x+cropSize]
+            # draw rects with mean value of color
+            meanCol = cv2.mean(crop)
+            b, g, r, _ = np.uint8(meanCol)
+            mCol = np.uint8([[[b, g, r]]])
+            scannerCol = MODULES.colorSelect(mCol)
+            thisColor = colors[scannerCol]
+
+            # draw rects with frame colored by range result
+            cv2.rectangle(dst, (x-1, y-1), (x+cropSize + 1, y+cropSize + 1),
+                          thisColor, 1)
+
+            # draw the mean color itself
+            cv2.rectangle(dst, (x, y), (x+cropSize,
+                                        y+cropSize), meanCol, -1)
+
+            cv2.putText(dst, str([counter, scannerCol]),
+                        (x-2, y-2), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.2, (0, 0, 0), lineType=cv2.LINE_AA)
+            # add colors to array for type analysis
+            colorArr[counter] = scannerCol
+            counter += 1
+
+    print('\n', '//////////////////////////////////', '\n',
+          colorArr.reshape(gridSize, gridSize).transpose())
+    # draw the video to screen
+    cv2.imshow("vid", dst)
+    # break video loop by pressing ESC
+    key = cv2.waitKey(10) & 0xFF
+    if key == 27:
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
